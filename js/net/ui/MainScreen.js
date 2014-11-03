d83e7bef212c64eba3a75e6fb4f22fa621c45fbc
Trygve <tnordberg@smm.org>
2014-11-03 12:24:19 -0600

    Cleaned up JS structure.

--- js/net/ui/MainScreen.js
+++ js/net/ui/MainScreen.js
@@ -4,7 +4,6 @@ define([ 'net/AppData', 'net/ui/ScreenManager', 'tween' ], function( AppData,  S
 
         this.containerDiv = containerDiv;
 
-        this.thumbGrid = {};
         this.init();
 
     }
@@ -12,56 +11,42 @@ define([ 'net/AppData', 'net/ui/ScreenManager', 'tween' ], function( AppData,  S
     // init() | Set up screen layout and buttons
     MainScreen.prototype.init = function() {
 
-        this.thumbGrid = new ThumbGrid( $("#main_nav_grid"), $("#main_nav_grid #thumb_clone"), $(AppData.configXML).find("component plants plant"), 5, 6 );
-        this.thumbGrid.makeGrid();
-
         this.refreshButtonListeners();
 
     };
 
-    // refresh() | Refresh displays as needed before showing
+    // refresh() | Refresh screen displays as needed before showing
     MainScreen.prototype.refresh = function() {
 
-        this.resetGrid ();
 
     };
 
-    // resetGrid() | Reset visual states of thumb grid
-    MainScreen.prototype.resetGrid = function() {
+    // featureSpecimen() | Transition to feature selected specimen
+    MainScreen.prototype.featureSpecimen = function( specId, specBtn ){
 
-        $("#main_nav_grid .thumb").each( function() {
-            TweenLite.set( $(this), { css: { zIndex: 0, opacity:1, scale:1, boxShadow:"0px 0px 0px 0px rgba(0,0,0,0)" } } );
-        });
+        AppData.setFeatureSpecimen( specId );
+
+        console.log('show spec: '+ specId);
 
     };
 
-    // transitionIn() | Tween in display elements
-    MainScreen.prototype.transitionIn = function( ){
+    // transitionInFeature() | Tween in display elements of featured specimen
+    MainScreen.prototype.transitionInFeature = function( ){
 
         TweenLite.delayedCall(0.75, function(){
 
-            TweenLite.to( $( "#main_nav_grid" ), 1, { css: { left:0 }, ease:Power2.easeOut } );
-            TweenLite.to( $( "#screen_main .content-left" ), 1, { css: { left:0 }, delay:0.2, ease:Power2.easeOut } );
+            TweenLite.to( $( "#main_nav" ), 1, { css: { left:0 }, ease:Power2.easeOut } );
+            TweenLite.to( $( "#screen_main .content-right" ), 1, { css: { left:0 }, delay:0.2, ease:Power2.easeOut } );
 
         });
 
     };
 
-    // transitionOut() | Tween out display elements
-    MainScreen.prototype.transitionOut = function( ){
+    // transitionOutFeature() | Tween out display elements of featured specimen
+    MainScreen.prototype.transitionOutFeature = function( ){
 
-        TweenLite.to( $( "#main_nav_grid" ), 1, { css: { left:-1080 }, ease:Power2.easeIn } );
-        TweenLite.to( $( "#screen_main .content-left" ), 1, { css: { left:-1080 }, delay:0.2, ease:Power2.easeIn } );
-
-    };
-
-    // featurePlant() | Transition to viewer screen with selected plant
-    MainScreen.prototype.featurePlant = function( plantId, plantThumbBtn ){
-
-        //Tell Viewer screen to setting up plant info
-        AppData.setFeaturePlant( plantId );
-
-        ScreenManager.showScreen( ScreenManager.SCREEN_VIEWER );
+        TweenLite.to( $( "#main_nav" ), 1, { css: { left:-1080 }, ease:Power2.easeIn } );
+        TweenLite.to( $( "#screen_main .content-right" ), 1, { css: { left:-1080 }, delay:0.2, ease:Power2.easeIn } );
 
     };
 
@@ -70,10 +55,10 @@ define([ 'net/AppData', 'net/ui/ScreenManager', 'tween' ], function( AppData,  S
 
         var thisRef = this;
 
-        //Removes all existing listeners to avoid doubling listeners
+        //Removes existing listeners
         this.disableButtonListeners();
 
-        //Listen for all button clicks...
+        //Listen for all button clicks
         $(this.containerDiv).find("[data-role='button']").on("click", function(event) {
 
             thisRef.buttonClicked( $(this).attr('id'), $(this) );
@@ -99,10 +84,10 @@ define([ 'net/AppData', 'net/ui/ScreenManager', 'tween' ], function( AppData,  S
         console.log("buttonClicked(btnId): " + btnId);
 
         //Plant Thumbs
-        if (btnId.substring(0, 6) == "thumb_") {
+        if (btnId.substring(0, 9) == "specimen_") {
 
-            var plantId = btnId.substring(6);
-            this.featurePlant(plantId, btnRef);
+            var specId = btnId.substring(9);
+            this.featureSpecimen(specId, btnRef);
             return;
 
         }
